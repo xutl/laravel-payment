aa6136854f15ab46d4e4c015f81374fce8ef9895
XuTongle <xutongle@gmail.com>
2018-12-20 14:31:46 +0800

    update

--- config/payment.php
+++ config/payment.php
@@ -11,20 +11,27 @@ return [
      */
     'channels' => [
 
-        //微信支付配置
+        /**
+         * 微信支付配置
+         */
         'wechat' => [
             'app_id' => '',
             'api_key' => '',
             'mch_id' => '',
             'private_key' => '',
             'public_key' => '',
-
             'timeout' => 5.0
         ],
 
-        //支付宝支付配置
+        /**
+         * 支付宝支付配置
+         */
         'alipay' => [
-
+            'app_id' => '',
+            'alipay_account' => '',
+            'private_key' => '',
+            'public_key' => '',
+            'timeout' => 5.0
         ],
     ],
 ];
--- src/Channels/Alipay.php
+++ src/Channels/Alipay.php
@@ -8,7 +8,6 @@
 namespace XuTL\Payment\Channels;
 
 use XuTL\Payment\ChannelInterface;
-use XuTL\Payment\Traits\Channel;
 
 /**
  * 支付宝支付渠道
@@ -17,7 +16,76 @@ use XuTL\Payment\Traits\Channel;
  */
 class Alipay implements ChannelInterface
 {
-    use Channel;
+
+    /**
+     * @var string 网关地址
+     */
+    public $endpoint = 'https://api.mch.weixin.qq.com';
+
+    const SIGNATURE_METHOD_RSA = 'RSA';
+    const SIGNATURE_METHOD_RSA2 = 'RSA2';
+
+    /**
+     * @var integer
+     */
+    public $appId;
+
+    /** @var string */
+    public $alipayAccount;
+
+    /**
+     * @var string 私钥
+     */
+    public $privateKey;
+
+    /**
+     * @var string 公钥
+     */
+    public $publicKey;
+
+    /**
+     * @var string 签名方法
+     */
+    public $signType = self::SIGNATURE_METHOD_RSA2;
+
+    /**
+     * 链接超时
+     * @var float
+     */
+    public $timeout = 5.0;
+
+    /**
+     * Wechat constructor.
+     * @param array $config
+     */
+    public function __construct($config = [])
+    {
+        if (!empty($config)) {
+            foreach ($config as $name => $value) {
+                $this->$name = $value;
+            }
+        }
+        $this->init();
+    }
+
+    /**
+     * Initializes the object.
+     * This method is invoked at the end of the constructor after the object is initialized with the
+     * given configuration.
+     */
+    public function init()
+    {
+
+    }
+
+    /**
+     * 返回网关地址
+     * @return string
+     */
+    public function getBaseUri()
+    {
+        return $this->endpoint;
+    }
 
     public function app(){
 
